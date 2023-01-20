31b06f74d284c004e9563f54798ad108eb2f48b5
너나들이 <tychejin@naver.com>
2023-01-20 14:38:45 +0900

    Feat: Redis 개발 실습

---
+++ hellowrold/src/main/java/com/example/hellowrold/HellowroldApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellowrold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowroldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellowroldApplication.class, args);
+  }
+}
