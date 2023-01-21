93ec2149bf6b08d1d8de11984471f162401535ff
너나들이 <tychejin@naver.com>
2023-01-21 16:09:14 +0900

    Feat: 서비스 속도를 높이는 캐시 레이어 만들기

---
+++ caching/src/main/java/com/example/caching/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.caching.controller;
+
+import com.example.caching.dto.UserProfile;
+import com.example.caching.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/users/{userId}/profile")
+  public UserProfile getUserProfile(@PathVariable(value = "userId") String userId) {
+    return userService.getUserProfile(userId);
+  }
+}
